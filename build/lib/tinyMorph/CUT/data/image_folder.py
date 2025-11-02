b85900ee10d543df3b8829df42fe71dd29bf51a4
Julien Ziolkowski <joe@macbookair.localdomain>
2025-11-02 17:18:18 +0100

    test commit

--- build/lib/tinyMorph/CUT/data/image_folder.py
+++ build/lib/tinyMorph/CUT/data/image_folder.py
@@ -2,6 +2,8 @@
 
 We modify the official PyTorch image folder (https://github.com/pytorch/vision/blob/master/torchvision/datasets/folder.py)
 so that this class can load images from both current directory and its subdirectories.
+
+Test change
 """
 
 import torch.utils.data as data
